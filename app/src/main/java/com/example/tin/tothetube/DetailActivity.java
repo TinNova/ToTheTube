5238abb45796fea5fc75fba798f81d0f7e5c0e14
Tin <tinnovamail@gmail.com>
2018-05-30 06:54:12 +0100

    feat: ArrivalTimes Intent to DetailActivity Created

---
+++ app/src/main/java/com/example/tin/tothetube/DetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.tin.tothetube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
--- app/src/main/java/com/example/tin/tothetube/MainActivity.java
+++ app/src/main/java/com/example/tin/tothetube/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tin.tothetube;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -50,23 +51,53 @@ public class MainActivity extends AppCompatActivity implements MainContract.Main
 
     }
 
+    ArrayList<Station> mStation;
+
     @Override
     public void showStation(ArrayList<Station> stations) {
+        mStation = stations;
         mAdapter.setStation(stations);
     }
 
     @Override
     public void tvArrivalTime0OnClick(View v, int position) {
-        Toast.makeText(this, "Position for 1st is " + position, Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, "Position for 1st is " + position + " Train Line: "
+                + mStation.get(position).getArrivals().get(0).getLineName(), Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("TimeToArrival", mStation.get(position).getArrivals().get(0).getTimeToStation());
+        intent.putExtra("LineName", mStation.get(position).getArrivals().get(0).getLineName());
+        intent.putExtra("LineId", mStation.get(position).getArrivals().get(0).getLineId());
+
+        startActivity(intent);
     }
 
     @Override
     public void tvArrivalTime1OnClick(View v, int position) {
-        Toast.makeText(this, "Position for 2nd is " + position, Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, "Position for 2nd is " + position + " Train Line: "
+                + mStation.get(position).getArrivals().get(1).getLineName(), Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("TimeToArrival", mStation.get(position).getArrivals().get(1).getTimeToStation());
+        intent.putExtra("LineName", mStation.get(position).getArrivals().get(1).getLineName());
+        intent.putExtra("LineId", mStation.get(position).getArrivals().get(1).getLineId());
+
+        startActivity(intent);
     }
 
     @Override
     public void tvArrivalTime2OnClick(View v, int position) {
-        Toast.makeText(this, "Position for 3rd is " + position, Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, "Position for 3rd is " + position + " Train Line: "
+                + mStation.get(position).getArrivals().get(2).getLineName(), Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("TimeToArrival", mStation.get(position).getArrivals().get(2).getTimeToStation());
+        intent.putExtra("LineName", mStation.get(position).getArrivals().get(2).getLineName());
+        intent.putExtra("LineId", mStation.get(position).getArrivals().get(2).getLineId());
+
+        startActivity(intent);
     }
 }
